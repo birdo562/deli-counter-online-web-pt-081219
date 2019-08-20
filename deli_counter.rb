0f20b70cbb7d5fd305f090b191faa7613d84cf85
Roberto Medina <robby.medina562@gmail.com>
2019-08-20 20:19:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,14 @@
 katz_deli = []
 
 def line(people)
-  if people.empty ?
-    puts "The line is currently empty"
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
   end
 end
-  
